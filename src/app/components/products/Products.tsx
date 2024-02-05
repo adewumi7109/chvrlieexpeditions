7941bb3f6490ecbe8f273c0dc29eda46e9b0313b
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-02-05 11:01:15 +0100

    fixes bug

--- src/app/components/products/Products.tsx
+++ src/app/components/products/Products.tsx
@@ -8,8 +8,8 @@ function Products() {
   <h1 className='mb-8 text-2xl font-bold'>NATURE PHOTOS</h1>
      <div className='grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-4 gap-9'>
         {products.map((product)=>(
-            <div >
-            <Product key={product.id} product = {product}/>
+            <div key={product.id}>
+            <Product  product = {product}/>
             </div>
         ))}
     </div>
