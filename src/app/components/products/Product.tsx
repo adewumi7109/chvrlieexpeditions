0fc1c34b883928606fd134c7e7f78e6780f2ddf3
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-02-08 17:15:34 +0100

    fixed image

--- src/app/components/products/Product.tsx
+++ src/app/components/products/Product.tsx
@@ -7,7 +7,7 @@ function Product({ product }: { product: any }) {
     return (
         <div key={product.id} className='w-[100%]  border-solid border border-textColor '>
           
-          <img src={product.image_url}  className="w-full" alt="" />
+          <img src={product.image_url} className="w-full max-h-[200px] object-cover" alt="" />
           <div className='flex p-4 justify-between items-center'>
                
                     <span>
@@ -20,7 +20,6 @@ function Product({ product }: { product: any }) {
                 
                 </span>
           </div>
-          {product?.image_url} 
           <button className='w-full  h-12 hover:bg-textColor hover:text-primaryColor bg-transparent border-solid border border-textColor '>Add to cart</button>
           {/* <div className='flex p-4 py-2 border-solid border border-textColor justify-between items-center'>
             <span><FaHeart size={22} /></span>
